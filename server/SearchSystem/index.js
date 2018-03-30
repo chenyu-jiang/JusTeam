a99c0d67ef5814ecbb4431ad1288902df9eb50e2
Micint0sh <1155092088@link.cuhk.edu.hk>
2018-03-30 17:19:20 +0800

    Added search function.

---
+++ server/SearchSystem/index.js
@@ -0,0 +1,51 @@
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200'
+});
+
+var indexName = "justeam";
+var teamTypeName = "team";
+var teamQueryFields = [];
+var postTypeName = "post";
+var postQueryFields = ["postTitle^3","content","tags^2"];
+
+function search(searchString, offset, limit) {
+    return new Promise(async (resolve, reject)=>{
+        var searchResult = [];
+        client.msearch({
+            body:[
+                {index:indexName, type:postTypeName},
+                {
+                    from: offset,
+                    size: limit,
+                    query: {
+                        multi_match: {
+                            query : searchString,
+                            fields : postQueryFields
+                        }
+                    }
+                }
+            ]
+        }, (err, response)=> {
+            if(err) reject(err);
+            else {
+                hitList = response["responses"][0]["hits"]["hits"];
+                for(var i=0;i<hitList.length;i++) {
+                    var item = {
+                        id : hitList[i]['_id'],
+                        content : hitList[i]["_source"]
+                    }
+                    searchResult.push(item);
+                }
+                resolve(searchResult);
+            }
+        });
+    });
+}
+
+async function test() {
+    var items = await search("Travel plan");
+    console.log(items);
+}
+
+test();
